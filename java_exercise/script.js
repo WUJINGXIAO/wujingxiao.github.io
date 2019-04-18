8bfeab587d4401b1f89e3fcdc9ea512ca8dcca45
Amanda Wu <jw5636@nyu.edu>
2019-04-18 11:48:41 -0400

    see the title

---
+++ java_exercise/script.js
@@ -0,0 +1,16 @@
+function funOne(){
+    var paragraphs= document.getElementsByTagName('p')
+    alert("There are "+paragraphs.length+" paragraphs in total.");
+    
+}
+
+function funTwo(){
+    var number=document.getElementsByClassName("div1")
+    alert("There are " +number.length+ " numbers.")
+}
+
+function funThree(){
+    var number2=document.getElementsByClassName("div2")
+    alert("There are "+number2.length+" fruit names.")
+    
+}
